973479d8aae0c2450336edec4996b223f2c4a89d
rodrigflorencia <rodrig.florencia@gmail.com>
2025-08-22 06:12:44 +0000

    dfs

--- Grafo/main.cpp
+++ Grafo/main.cpp
@@ -1,4 +1,4 @@
-#include "Grafo.tpp"
+#include "dfs.tpp"
 
 using namespace std;
 int main()
@@ -17,6 +17,12 @@ int main()
   cout << "Hay A-B? " << g.hayArco('A', 'B') << "\n";
   cout << "Hay C-B? " << g.hayArco('C', 'B') << "\n";
   cout << "Peso A-C = " << *(g.getCosto('A', 'C')) << "\n";
+
+  dfsForestArcos(g);      // clasificación de arcos
+  dfsForestSimple(g);     // recorrido/orden
+
+  bool ciclo = hayCicloSimple(g);
+  cout << "Hay ciclo? " << (ciclo ? "si" : "no") << "\n";
 
   return 0;
 
