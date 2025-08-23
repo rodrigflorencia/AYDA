46f9285ef852de1d518d3fc8a93a5ba5a377137b
rodrigflorencia <rodrig.florencia@gmail.com>
2025-08-23 03:15:09 +0000

    fix cout

--- BFS/Grafo.hpp
+++ BFS/Grafo.hpp
@@ -7,44 +7,44 @@
 #include <iostream>
 using namespace std;
 
-template <class V>
-class Grafo
-{
+template <class V> class Grafo {
 public:
-    Grafo(); // por defecto se asume dirigido
-    virtual ~Grafo();
-    Grafo(bool noDirigido);
+  Grafo(); // por defecto se asume dirigido
+  virtual ~Grafo();
+  Grafo(bool noDirigido);
 
-    bool addVertice(const V &vertice);
-    bool delVertice(const V &vertice); // TODO borrar arcos que inciden en vertice y luego borrar vertice
-    set<V> getVertices() const;
+  bool addVertice(const V &vertice);
+  bool delVertice(const V &vertice); // TODO borrar arcos que inciden en vertice
+                                     // y luego borrar vertice
+  set<V> getVertices() const;
 
-    bool addArco(const V &deVertice, const V &aVertice);
-    bool delArco(const V &deVertice, const V &aVertice);
-    bool hayArco(const V &deVertice, const V &aVertice) const;
-    set<V> getAdyacentes(const V &vertice) const;
+  bool addArco(const V &deVertice, const V &aVertice);
+  bool delArco(const V &deVertice, const V &aVertice);
+  bool hayArco(const V &deVertice, const V &aVertice) const;
+  set<V> getAdyacentes(const V &vertice) const;
 
-    int nVertices() const;
-    int getGradoSalida(const V &v) const;
-    int getGrado(const V &v) const;
+  int nVertices() const;
+  int getGradoSalida(const V &v) const;
+  int getGrado(const V &v) const;
 
-    friend  ostream& operator<<(ostream& os, const Grafo& g)
-    {
-        for (typename map<V, set<V>>::const_iterator it = g.grafoMapa.begin(); it != g.grafoMapa.end(); it++)
-    {
+  friend ostream &operator<<(ostream &os, const Grafo &g) {
+    for (typename map<V, set<V>>::const_iterator it = g.grafoMapa.begin();
+         it != g.grafoMapa.end(); it++) {
 
-        cout<<it->first<<": { ";
-        for (typename set<V>::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++)
-            cout<<*it2<<" ; ";
-        cout<<" } \n";
-
+      os << it->first << ": { ";
+      for (typename set<V>::const_iterator it2 = it->second.begin();
+           it2 != it->second.end(); it2++)
+        os << *it2 << " ; ";
+      os << " } \n";
     }
-    };
+    return os;
+  }
 
 private:
-    
-    bool noDirigido; // No se hace esto de usar un flag para GD/GND según objetos. Deberíamos tener una clase Grafo y dos clases hijas GD y GND, pero será en el próximo cuatrimestre.
-    map<V, set<V>> grafoMapa;
+  bool noDirigido; // No se hace esto de usar un flag para GD/GND según objetos.
+                   // Deberíamos tener una clase Grafo y dos clases hijas GD y
+                   // GND, pero será en el próximo cuatrimestre.
+  map<V, set<V>> grafoMapa;
 };
 
 #endif
